a4e6fcebe7f72447a53012869fae8f2734b17af4
Adam <admchristopher@gmail.com>
2019-08-26 18:23:19 -0700

    update

--- updated_scraper.py
+++ updated_scraper.py
@@ -5,11 +5,27 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 # opening up the connection and grabbing the page
-my_url = "https://www.espn.com"
+my_url = "https://www.espn.com/"
 uClient = uReq(my_url)
 page_html = uClient.read()
 uClient.close()
 
 # html parsing
 page_soup = soup(page_html, "html.parser")
-quick_links = page_soup.findAll("li", {"class":"quicklinks_list__item"})
+head_lines = page_soup.findAll("section", {"class":"headlineStack__listContainer"})
+
+filename = "sports_today.csv"
+f = open(filename, "w")
+
+headers = "title, link\n"
+
+f.write(headers)
+
+story = head_lines[0]
+for story in head_lines:
+      title = story.li.a.text
+      link = story.li.a["href"]
+
+      print("Story: " + title)
+      print("Link: " + link)
+      f.write(title + "," + link + "\n")
