0b82159a1c960b927ec9419b1c63b5d347ffbc98
Adam Reeves <48875460+irish1uck@users.noreply.github.com>
2019-08-26 18:31:42 -0700

    Create espy_scraper.py

---
+++ espy_scraper.py
@@ -0,0 +1,31 @@
+
+
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+# opening up the connection and grabbing the page
+my_url = "https://www.espn.com/"
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+head_lines = page_soup.findAll("section", {"class":"headlineStack__listContainer"})
+
+filename = "sports_today.csv"
+f = open(filename, "w")
+
+headers = "title, link\n"
+
+f.write(headers)
+
+story = head_lines[0]
+for story in head_lines:
+      title = story.li.a.text
+      link = story.li.a["href"]
+
+      print("Story: " + title)
+      print("Link: " + link)
+      f.write(title + "," + link + "\n")
